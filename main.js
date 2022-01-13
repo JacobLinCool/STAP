c9efb33edec4f92bbf80ed321f6e90fa76c6f9d4
JacobLinCool <jacoblincool@gmail.com>
2022-01-14 06:35:58 +0800

    perf: use web worker to increase performance

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
 import "./style.css";
-import { Analyzer, Dictionary, sort_map } from "textual-data-analysis";
+import Worker from "./worker?worker&inline";
+
+const worker = new Worker();
 
 let cooldown = 0;
 
 document.querySelector("#file").addEventListener("drop", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
+    e.preventDefault();
+    e.stopPropagation();
 
-  const file = e.dataTransfer.files[0];
+    const file = e.dataTransfer.files[0];
 
-  let file_content = "";
+    let file_content = "";
 
-  if (file.type.startsWith("text")) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      file_content = e.target.result;
-      document.querySelector("#file-content").value = file_content;
+    if (file.type.startsWith("text")) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            file_content = e.target.result;
+            document.querySelector("#file-content").value = file_content;
 
-      parse();
-    };
-    reader.readAsText(file);
-  }
+            parse();
+        };
+        reader.readAsText(file);
+    }
 });
 
 document.querySelector("#file-content").addEventListener("input", parse);
 
-function parse() {
-  if (cooldown > Date.now()) return;
-  cooldown = Date.now() + 1000;
-  const file_content = document.querySelector("#file-content").value;
-  const size = file_content.length;
+async function parse() {
+    if (cooldown > Date.now()) return;
+    cooldown = Date.now() + 1000;
 
-  console.time("parse: " + size);
+    const file_content = document.querySelector("#file-content").value;
+    const size = file_content.length;
 
-  const analyzer = new Analyzer(file_content).split(Dictionary.punctuations);
-  const words = sort_map(analyzer.meaningful());
+    console.time("parse: " + size);
 
-  console.log(words);
-  console.timeEnd("parse: " + size);
+    const words = new Promise((r) => {
+        worker.onmessage = function (e) {
+            r(e.data);
+        };
+    });
 
-  document.querySelector("#meaningful-words").value = `Map(${words.size}) {\n${[
-    ...words.entries(),
-  ].reduce((acc, [word, count]) => acc + `    '${word}' => ${count},\n`, "")}}`;
+    worker.postMessage(file_content);
+
+    console.log(await words);
+    console.timeEnd("parse: " + size);
+
+    document.querySelector("#meaningful-words").value = `Map(${(await words).size}) {\n${[
+        ...(await words).entries(),
+    ].reduce((acc, [word, count]) => acc + `    '${word}' => ${count},\n`, "")}}`;
 }
-
-window.Analyzer = Analyzer;
-window.Dictionary = Dictionary;
